64bee93de689c38e5cf08504b814aaa2984356ef
supergometan <max-g@gmx.net>
2018-06-23 01:55:37 +0200

    t

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
-
+/*
 var GameServer = require("./server/gameserver")
 
 console.log(ip)
@@ -64,8 +64,7 @@ webSocketServer.on("connection", function(socket) {
 	})
 	
 })
-
-/*
+*/
 //  OpenShift sample Node application
 var express = require('express'),
     app     = express(),
@@ -75,11 +74,6 @@ Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
-
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
 
 if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
   var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
@@ -174,6 +168,3 @@ app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
 
 module.exports = app ;
-
-
-*/
